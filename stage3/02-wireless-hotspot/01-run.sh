5dec40e621547d797f947afe91b0f75f0ab52c4d
Zane Mountcastle <zane.mountcastle@gmail.com>
2018-06-22 22:46:05 -0700

    Formatting

--- stage3/02-wireless-hotspot/01-run.sh
+++ stage3/02-wireless-hotspot/01-run.sh
@@ -7,12 +7,12 @@ rm -f  "${ROOTFS_DIR}/etc/default/hostapd"
 rm -f  "${ROOTFS_DIR}/etc/rc.local"
 rm -f  "${ROOTFS_DIR}/etc/sysctl.conf"
 
-install -m 644 -v files/dhcpcd.conf  "${ROOTFS_DIR}/etc/dhcpcd.conf"
-install -m 644 -v files/dnsmasq.conf "${ROOTFS_DIR}/etc/dnsmasq.conf"
-install -m 644 -v files/hostapd.conf "${ROOTFS_DIR}/etc/hostapd/hostapd.conf"
-install -m 644 -v files/hostapd      "${ROOTFS_DIR}/etc/default/hostapd"
-install -m 755 -v files/rc.local     "${ROOTFS_DIR}/etc/rc.local"
-install -m 644 -v files/sysctl.conf  "${ROOTFS_DIR}/etc/sysctl.conf"
+install -m 644 -v files/dhcpcd.conf "${ROOTFS_DIR}/etc/dhcpcd.conf"
+install -m 644 -v files/dnsmasq.conf  "${ROOTFS_DIR}/etc/dnsmasq.conf"
+install -m 644 -v files/hostapd.conf  "${ROOTFS_DIR}/etc/hostapd/hostapd.conf"
+install -m 644 -v files/hostapd "${ROOTFS_DIR}/etc/default/hostapd"
+install -m 755 -v files/rc.local  "${ROOTFS_DIR}/etc/rc.local"
+install -m 644 -v files/sysctl.conf "${ROOTFS_DIR}/etc/sysctl.conf"
 
 on_chroot << EOF
 

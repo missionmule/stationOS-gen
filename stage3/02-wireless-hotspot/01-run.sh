cf429550949719a3e05b170e28ebe9f55439d2ff
Zane Mountcastle <zane.mountcastle@gmail.com>
2018-06-24 17:17:35 -0700

    Minor syntax changes

--- stage3/02-wireless-hotspot/01-run.sh
+++ stage3/02-wireless-hotspot/01-run.sh
@@ -7,7 +7,6 @@ on_chroot << EOF
 rm -f "${ROOTFS_DIR}/etc/dhcpcd.conf"
 rm -f  "${ROOTFS_DIR}/etc/dnsmasq.conf"
 rm -f  "${ROOTFS_DIR}/etc/default/hostapd"
-rm -f  "${ROOTFS_DIR}/etc/rc.local"
 rm -f  "${ROOTFS_DIR}/etc/sysctl.conf"
 
 EOF
@@ -17,22 +16,22 @@ echo "IP configuration files removal complete"
 echo "Configuring wireless access point..."
 
 echo "Installing dhcpcd.conf"
-install -m 644 -v files/dhcpcd.conf "${ROOTFS_DIR}/etc/dhcpcd.conf"
+install -m 644 -v files/dhcpcd.conf "${ROOTFS_DIR}/etc/"
 
 echo "Installing dnsmasq.conf"
-install -m 644 -v files/dnsmasq.conf  "${ROOTFS_DIR}/etc/dnsmasq.conf"
+install -m 644 -v files/dnsmasq.conf  "${ROOTFS_DIR}/etc/"
 
 echo "Installing hostapd.conf"
-install -m 644 -v files/hostapd.conf  "${ROOTFS_DIR}/etc/hostapd/hostapd.conf"
+install -m 644 -v files/hostapd.conf  "${ROOTFS_DIR}/etc/hostapd/"
 
 echo "Installing hostapd"
-install -m 755 -v files/hostapd "${ROOTFS_DIR}/etc/default/hostapd"
+install -m 755 -v files/hostapd "${ROOTFS_DIR}/etc/default/"
 
-echo "Installing rc.local"
-install -m 755 -v files/rc.local  "${ROOTFS_DIR}/etc/rc.local"
+# echo "Installing rc.local"
+# install -m 755 -v files/rc.local  "${ROOTFS_DIR}/etc/"
 
 echo "Installing sysctl.conf"
-install -m 644 -v files/sysctl.conf "${ROOTFS_DIR}/etc/sysctl.conf"
+install -m 644 -v files/sysctl.conf "${ROOTFS_DIR}/etc/"
 
 echo "Wiress access point configuration complete"
 

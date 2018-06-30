0622cf53adfbed3c5a13482b7d0300077e6cea15
Zane Mountcastle <zane.mountcastle@gmail.com>
2018-06-29 21:59:30 -0700

    Ensure all Python package requirements are installed

--- stage3/01-companion-computer-avionics/01-run.sh
+++ stage3/01-companion-computer-avionics/01-run.sh
@@ -7,6 +7,8 @@ mkdir -p /opt/mission-mule
 
 cd /opt/mission-mule && git clone -v https://github.com/missionmule/firefly-mule.git
 
+cd /opt/mission-mule/firefly-mule && pip3 install -r /opt/mission-mule/firefly-mule/requirements.txt
+
 EOF
 
 install -m 644 files/mission-mule-avionics.service   "${ROOTFS_DIR}/lib/systemd/system/"

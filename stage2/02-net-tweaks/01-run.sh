d774148c593d932e275e38b4921d20fa8ee60516
Zane Mountcastle <zane.mountcastle@gmail.com>
2018-08-03 17:51:43 -0700

    Moved usbmount config to net tweaks

--- stage2/02-net-tweaks/01-run.sh
+++ stage2/02-net-tweaks/01-run.sh
@@ -6,5 +6,13 @@ install -v -m 644 files/wait.conf		"${ROOTFS_DIR}/etc/systemd/system/dhcpcd.serv
 install -v -d					"${ROOTFS_DIR}/etc/wpa_supplicant"
 install -v -m 600 files/wpa_supplicant.conf	"${ROOTFS_DIR}/etc/wpa_supplicant/"
 
+echo "Installing Jessie backwards compatible systemd-udevd service config"
+install -v -d					"${ROOTFS_DIR}/lib/systemd/system/"
+install -m 644 files/systemd-udevd.service   "${ROOTFS_DIR}/lib/systemd/system/"
+
+echo "Installing usbmount custom config"
+install -v -d					"${ROOTFS_DIR}/lib/systemd/usbmount/"
+install -m 644 files/usbmount.conf  "${ROOTFS_DIR}/etc/usbmount/"
+
 echo "Installing 72-wlan-geo-dependent.rules"
 install -m 644 -v files/72-wlan-geo-dependent.rules "${ROOTFS_DIR}/etc/udev/rules.d/"
--- stage3/02-automount-peripheral-filesystems/01-run.sh
+++ stage3/02-automount-peripheral-filesystems/01-run.sh
@@ -1,11 +1,18 @@
 #!/bin/bash -e
 
-on_chroot << EOF
-
-rm -rf /lib/systemd/system/systemd-udevd.service
-rm -rf /etc/usbmount/usbmount.conf
-
-EOF
-
-install -m 644 files/systemd-udevd.service   "${ROOTFS_DIR}/lib/systemd/system/"
-install -m 644 files/usbmount.conf  "${ROOTFS_DIR}/etc/usbmount/"
+# on_chroot << EOF
+#
+# rm -rf /lib/systemd/system/systemd-udevd.service
+# rm -rf /etc/usbmount/usbmount.conf
+#
+# EOF
+#
+#
+# install -v -d					 "${ROOTFS_DIR}/lib/systemd/system/"
+# install -v -m 644 files/wpa_supplicant.conf	"${ROOTFS_DIR}/etc/wpa_supplicant/"
+#
+# install -v -d					"${ROOTFS_DIR}/etc/wpa_supplicant"
+# install -v -m 644 files/wpa_supplicant.conf	"${ROOTFS_DIR}/etc/wpa_supplicant/"
+#
+# install -m 644 files/systemd-udevd.service   "${ROOTFS_DIR}/lib/systemd/system/"
+# install -m 644 files/usbmount.conf  "${ROOTFS_DIR}/etc/usbmount/"
